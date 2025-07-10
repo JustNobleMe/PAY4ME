609f2faa7b060789d6537ecd0cbb3982628cc04a
JustNobleMe <isaacodey01@gmail.com>
2025-07-10 15:28:05 -0700

    adding a loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import Nav from './components/Nav';
 import Header from './components/Header';
 import Partners from './components/Partners';
@@ -5,8 +6,33 @@ import Support from './components/Support';
 import FAQs from './components/FAQs';
 import Footer from './components/Footer';
 import Mobile from './components/mobile';
+import Loader from './components/Loader';
 
 function App() {
+   const [isLoaded, setIsLoaded] = useState(false);
+
+    useEffect(() => {
+
+        const handleLoad = () => {
+            setIsLoaded(true);
+        }
+
+        if (document.readyState === 'complete') {
+            handleLoad()
+        } else {
+            window.addEventListener('load', handleLoad);
+        }
+        return () => {
+            window.removeEventListener('load', handleLoad);
+        };
+    }, []);
+
+    if (!isLoaded) {
+        return (
+            <Loader />
+        )
+    }
+
   return (
     <>
     <div className='hidden max-lg:block'>
---
+++ src/components/Loader.tsx
@@ -0,0 +1,8 @@
+export default function Loader() {
+  return (
+    <div className='h-screen w-screen flex flex-col gap-2 items-center justify-center bg-white'>
+        <div className='bg-[#74bf24] h-15 w-15 rounded animate-bounce'></div>
+        <p className='font-bold text-2xl drop-shadow'>PAY4ME</p>
+    </div>
+  )
+}
